6fb3db4bc310329fbae7899e57aefe20ac20c476
ManuelFajardo78 <57449214+ManuelFajardo78@users.noreply.github.com>
2020-09-16 06:18:15 -0500

    Update ComentarioControl.java

--- BackEND/restPublicaciones/src/main/java/com/example/demo/rest/ComentarioControl.java
+++ BackEND/restPublicaciones/src/main/java/com/example/demo/rest/ComentarioControl.java
@@ -7,11 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Comentario;
@@ -19,14 +17,14 @@ import com.example.demo.service.ComentarioService;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping()
+@RequestMapping("/api")
 public class ComentarioControl {
 	@Autowired
 	ComentarioService comentarioS;
 	@PostMapping
 	@CrossOrigin
 	public ResponseEntity saveComentario(@RequestBody Comentario comentario) {
-		usuarioS.crearComentario(comentario);
+		comentarioS.crearComentario(comentario);
 		return ResponseEntity.ok("OK");
 	}
 	
